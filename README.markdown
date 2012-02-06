35260c4a97727347a96bb2b438d86574561453e8
Anton <Anton_olsson96@hotmail.com>
2012-02-06 13:22:54 +0100

    Skapat div filer

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# kod och anteckningar programmering A vt 2012 #
+
+Här finns allt som jag gör under lektioner, exklusive inlämningar.
---
+++ scratchpad 1.js
@@ -0,0 +1,25 @@
+/*
+ * Det här är ett JavaScript-kladdblock.
+ *
+ * Skriv in lite JavaScript, högerklicka eller välj från Kör-menyn:
+ * 1. Kör för att evaluera den markerade texten,
+ * 2. Inspektera för att öppna en objektinspektör på resultatet, eller
+ * 3. Visa för att infoga resultatet i en kommentar efter markeringen.
+ */
+
+//var name = prompt("Vad heter du?");
+//alert("Ditt namn är " + name.length + " tecken långt");
+
+//uppgift b - räkna ut någons ålder
+//var year = prompt("Vilket år är du född (4siffror)?");
+//var age = (new Date()).getFullYear() - year;
+//alert("Du fyller (eller fyllde) " + age + " år i år");
+//CTRL R
+
+//alert (3+5); // 8
+// CTRL R
+
+var name = prompt("Vad heter du?");
+alert("Första bokstaven i ditt namn är " + name[0]);
+alert("Sista bokstaven är " + name[name.length - 1]);
+
